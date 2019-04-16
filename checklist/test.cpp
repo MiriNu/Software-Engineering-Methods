a8087e6dbaadd41474e5a01d15312ff5e5d29350
MiriNu <miryamnudelman@gmail.com>
2019-04-16 20:51:26 +0300

    Initial test

---
+++ checklist/test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>  
+#include <windows.h>
+
+using namespace std; 
+  
+// main function - 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+    MessageBoxA(NULL, "Hello", "Hello", NULL);
+      
+    return 0; 
+} 
