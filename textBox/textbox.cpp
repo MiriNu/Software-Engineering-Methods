f8b3f8018ed43f4f76ec91341dd3b596723e2278
GoldenDude <edanazran@gmail.com>
2019-04-24 16:55:15 +0300

    box now grows and shrinks

--- textBox/textbox.cpp
+++ textBox/textbox.cpp
@@ -2,11 +2,25 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+
 TextBox::TextBox(short width, short top, short left) : 
     width(width), top(top), left(left), hasBorder(true), value(""), 
     color(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY){
         currentCoord = {left + 1 + value.length(), top + 1};
+        oldWidth = width;
     }
+
+void TextBox:: handleMouseEvent(MOUSE_EVENT_RECORD& event){
+    std::ofstream tmp;
+    tmp.open("test.txt");
+    tmp << "here";
+    if(event.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED){
+        auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        currentCoord = event.dwMousePosition;
+        tmp << "here2";
+        SetConsoleCursorPosition(handle, currentCoord);
+    }
+}
 
 void TextBox:: handleKeyboardEvent(KEY_EVENT_RECORD& event){
     int textWidth = value.length();
@@ -48,8 +62,10 @@ void TextBox:: handleKeyboardEvent(KEY_EVENT_RECORD& event){
                 currentCoord = { currentCoord.X - 1, currentCoord.Y };
             }
         }
-        if(this->value.length() < width)
-            width - 2 > 12 ? width -= 2 : 12;
+        if(this->value.length() < width){
+            oldWidth = width;
+            width - 2 > 12 ? width -= 1 : 12;
+        }
         
         this->draw();
     }
@@ -67,17 +83,24 @@ void TextBox::draw(){
 
     std::cout << (char)0xbf;
 
+    for(int i = width; i < oldWidth; ++i)
+        std::cout << " ";
+
     SetConsoleCursorPosition(handle,{ coord.X, coord.Y + 1 });
     std::cout << (char)0xb3;
 
     for(int i = 0; i < value.length(); ++i)
         std::cout << value[i];
     
-    for(int i = value.length() + 1; i < width; ++i)
+    for(int i = value.length(); i < width; ++i)
+        std::cout << " ";
+    
+    for(int i = width; i < oldWidth + 1; ++i)
         std::cout << " ";
 
     SetConsoleCursorPosition(handle,{ left + width + 1, top + 1 });
     std::cout << (char)0xb3;
+
 
     SetConsoleCursorPosition(handle,{ left, top + 2 });
     std::cout << (char)0xc0;
@@ -86,5 +109,9 @@ void TextBox::draw(){
         std::cout << (char)0xc4;  
 
     std::cout << (char)0xd9;
+        
+    for(int i = width; i < oldWidth; ++i)
+        std::cout << " ";
+
     SetConsoleCursorPosition(handle, currentCoord);
 }
