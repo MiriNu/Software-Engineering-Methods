5a52c3a0e9f8f3e425cc596af162433239fb2c8d
bararbel@gmail.com <bararbel@gmail.com>
2019-07-20 18:54:21 +0300

    nullborder

--- FinalProject/Common/Border/NullBorder.h
+++ FinalProject/Common/Border/NullBorder.h
@@ -5,5 +5,4 @@ class NullBorder : public Border {
     public:
         NullBorder();
         virtual ~NullBorder();
-        virtual void drawBorder(Graphics& g, short left, short top, short width, short height);
 };
