023b8a330c8283b17f7fd8fe67a20b57924d8525
GoldenDude <edanazran@gmail.com>
2019-07-23 14:12:39 +0300

    finished numeric box?

--- FinalProject/Controls/NumericBox.cpp
+++ FinalProject/Controls/NumericBox.cpp
@@ -2,7 +2,7 @@
 
 NumericBox::NumericBox(short left, short top, short width, short height, Border* border, Color textColor, Color backgroundColor) :
     Control(left, top, width + 1, height + 1, new SingleBorder(), textColor, backgroundColor),
-    value(left + 7, top + 1, 1, 1, new SingleBorder(), textColor, backgroundColor, " 0"),
+    value(left + 7, top + 1, 1, 1, new SingleBorder(), textColor, backgroundColor, "0"),
     add(left + 2, top + 1, 1, 1, new SingleBorder(), textColor, backgroundColor, " +"),
     subtract(left + 12, top + 1, 1, 1, new SingleBorder(), textColor, backgroundColor, " -")
 {
@@ -28,43 +28,37 @@ void NumericBox::update(int x, int y){
 }
 
 void NumericBox::addToVal(){
-    string temp = value.getValue();
-    string temp2 = " ";
-    int numericVal = atoi(temp.c_str());
+    int numericVal = atoi(value.getValue().c_str());
     ++numericVal;
-    temp2.append(to_string(numericVal));
-    value.setValue(temp2);
+    value.setValue(to_string(numericVal));
 }
 
 void NumericBox::subFromVal(){
-    string temp = value.getValue();
-    string temp2 = " ";
-    int numericVal = atoi(temp.c_str());
+    int numericVal = atoi(value.getValue().c_str());
     --numericVal;
-    temp2.append(to_string(numericVal));
-    value.setValue(temp2);
+    value.setValue(to_string(numericVal));
 }
 
 void NumericBox::mousePressed(int x, int y, bool isLeft) {
-    add.mousePressed(x - 7, y - 6, isLeft);
-    subtract.mousePressed(x - 7, y - 6, isLeft);
+    add.mousePressed(x, y, isLeft);
+    subtract.mousePressed(x, y, isLeft);
 }
 
 void NumericBox::draw(Graphics& g, int x, int y, size_t z){
     short relativeX, relativeY;
-    // Control::draw(g, x, y, z);
+    if(z == 0){
+    Control::draw(g, x, y, z);
 
     relativeX = value.getLeft();
     relativeY = value.getTop();
-    value.draw(g, x + relativeX + 1, y + relativeY + 1, z);
-    // value.draw(g, relativeX, relativeY, z);
+    value.draw(g, relativeX, relativeY, z);
+
     relativeX = add.getLeft();
     relativeY = add.getTop();
-    add.draw(g, x + relativeX + 1, y + relativeY + 1, z);
+    add.draw(g, relativeX, relativeY, z);
 
-    // add.draw(g, relativeX, relativeY, z);
     relativeX = subtract.getLeft();
     relativeY = subtract.getTop();
-    // subtract.draw(g, relativeX, relativeY, z);
-    subtract.draw(g, x + relativeX + 1, y + relativeY + 1, z);
+    subtract.draw(g, relativeX, relativeY, z);
+    }
 }
